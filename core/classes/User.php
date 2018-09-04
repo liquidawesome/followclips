d60c08d108f69e637c1c26e878df8cfd81f740c0
Sean McCartan <sean2223@gmail.com>
2018-09-04 19:26:46 -0400

    Add getUsername method, begin get following method. Fix display logic in index.php

--- core/classes/User.php
+++ core/classes/User.php
@@ -16,13 +16,12 @@ class User {
 		$rsp = curl_exec($curlUser);
 		
 		// Get info for debug purposes
-		$info = curl_getinfo($curlUser);
+		//$info = curl_getinfo($curlUser);
 		
 		// Close cURL connection
 		curl_close($curlUser);
 		
 		$rsp = json_decode($rsp,true);
-		//var_dump($rsp);
 		
 		try {
 			if (array_key_exists(0, $rsp['data']) == true) {
@@ -41,9 +40,60 @@ class User {
 		return $id;
 	}
 	
-	public function getUsername() {
-		$username = $this->details['display_name'];
-		return $username;
+	public function getUsernameFromId($id) {
+		$url = 'https://api.twitch.tv/helix/users?id='.$id;
+		
+		// Initiate cURL
+		$ch = curl_init($url);
+		
+		// Set cURL options
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Client-ID: usc7ke0v80ye96khi6jhia4w61i8yr'));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		
+		// Execute request, decode json response into assoc array
+		$rsp = curl_exec($ch);
+		
+		// Close cURL connection
+		curl_close($ch);
+		
+		$rsp = json_decode($rsp,true);
+		return $rsp['data'][0]['display_name'];
+	}
+	
+	public function following() {
+		$id = $this->getId();
+		$after = '';
+		$users = array();
+		do {
+			$url = 'https://api.twitch.tv/helix/users/follows?from_id='.$id.'&after='.$after;
+			
+			// Initiate cURL
+			$ch = curl_init($url);
+			
+			// Set cURL options
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array('Client-ID: usc7ke0v80ye96khi6jhia4w61i8yr'));
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			
+			// Execute request, decode json response into assoc array
+			$rsp = curl_exec($ch);
+			
+			// Get info for debug purposes
+			//$info = curl_getinfo($ch);
+			
+			// Close cURL connection
+			curl_close($ch);
+			
+			$rsp = json_decode($rsp,true);
+			//var_dump($rsp);
+			
+			foreach($rsp['data'] as $follow) {
+				$users[] = $follow['to_id'];
+			}
+			
+			$after = $rsp['pagination']['cursor'];
+			
+		} while(!empty($rsp['data']));
+		var_dump($users);
 	}
 
 }
--- index.php
+++ index.php
@@ -2,23 +2,33 @@
 ini_set('display_errors', 1);
 require 'core/init.php';
 if (isset($_GET['name']) === true && empty($_GET['name']) === false) {
+	$entered = true;
 	$username = htmlspecialchars($_GET['name']);
 	
 	$user = new User($username);
 	
-	echo 'ID: ',$user->getId();
-	echo '<br/>ID: ',$user->getUsername();
+	if ($user->getId() !== null) {
+		echo 'ID: ',$user->getUsernameFromId($user->getId());
+	}
+	
+	$user->following();
 	
 	// Process data
 	//$id = $rsp['id'];
 	//var_dump($rsp);
+} else {
+	$entered = false;
 }
 
 ?>
 <html>
 <body>
 	<h1>Your Followed User Clips</h1>
+	<?php if ($entered == false): ?>
 	<p>Enter your Twitch.tv username to continue:</p>
+	<?php else: ?>
+	<p>Welcome, <?php echo $user->getUsernameFromId($user->getId()); ?></p>
+	<?php endif; ?>
 	<form action="" method="get">
 	<input type="text" name="name"/>
 	<input type="submit" value="Continue..."/>
